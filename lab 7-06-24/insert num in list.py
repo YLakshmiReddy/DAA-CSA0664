befdee3ca207581293c8380cadfb9f68f47f96a2
YLakshmiReddy <154490280+YLakshmiReddy@users.noreply.github.com>
2024-06-07 14:15:08 +0530

    Add files via upload

---
+++ lab 7-06-24/insert num in list.py
@@ -0,0 +1,8 @@
+def insert_number_in_list(lst, num, index):
+    lst.insert(index, num)
+    return lst
+my_list = [1, 2, 3, 4, 5]
+number_to_insert = 10
+insertion_index = 2
+result_list = insert_number_in_list(my_list, number_to_insert, insertion_index)
+print(result_list)
---
+++ lab 7-06-24/max min list.py
@@ -0,0 +1,6 @@
+numbers = [3, 7, 1, 9, 4, 6]
+for num in numbers:
+    print(f"Number: {num}")
+    print(f"Minimum: {min(str(num))}")
+    print(f"Maximum: {max(str(num))}")
+    print()
